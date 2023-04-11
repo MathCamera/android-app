8ff2f10174210677181fd61d9009ada413b13685
Dmitry Karpenko <dkarp3ncko@yandex.ru>
2023-04-11 19:12:00 +0500

    pre-release 0.4.0

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ from modules.xcamera.platform_api import PORTRAIT, set_orientation,check_flashli
 from modules.urlrequest.urlrequest import UrlRequest
 from modules.plotting import render_plot
 
-import base64,os,certifi,urllib.parse,json,webbrowser,shutil
+import base64,os,certifi,urllib.parse,json,webbrowser,shutil,math
 from PIL import Image
 from io import BytesIO
 
@@ -30,24 +30,19 @@ if platform != "android":
 class MathCamera(MDApp):
     main_col = "#02714C"
     prev_screen_data = {"sc_name":"sc_text","sc_title":"Главная"}
-    settings = json.load(open('settings.json'))
-
     def build(self):
-        self.theme_cls.theme_style = "Dark" if self.settings["dark_theme"] == True else "Light"
         self.theme_cls.primary_palette = "Green"
         self.theme_cls.material_style = "M2"
 
-        Window.bind(on_keyboard=self.key_handler)
-
         return Builder.load_file('md.kv')
     
-    #Функция вызывается при окончании загрузки
     def on_start(self):
-        pass
+        self.settings = json.load(open('settings.json'))
+        self.theme_cls.theme_style = "Dark" if self.settings["dark_theme"] == True else "Light"
+        Window.bind(on_keyboard=self.key_handler)
 
     def clear_cache(self):
         paths = ['mpl_tmp','xcamera_tmp']
-        total_size = 0
         for path_name in paths:
             if os.path.exists(path_name):
                 shutil.rmtree(path_name, ignore_errors=True)
@@ -59,9 +54,6 @@ class MathCamera(MDApp):
         settings = self.settings
         for elem_id in settings.keys():
             ids[elem_id].active = settings[elem_id]
-
-        #Убираем переключение темы, пока не добавим тёмную тему
-        #ids["dark_theme"].disabled = True
 
         ids["auto_flashlight"].disabled = not(ids["enable_flashlight"].active)
 
@@ -162,7 +154,6 @@ class MathCamera(MDApp):
         self.root.ids['tb'].title = title
         self.root.ids['nav_drawer'].set_state("closed")
 
-        #Костыль для деактивации поля ввода задачи (sc_text>text_field)
         self.root.ids.textarea.ids.text_field.focus = False
 
     def edit_textfield(self,text,move_cursor=0):
@@ -179,50 +170,60 @@ class MathCamera(MDApp):
         textfield.cursor = (cursor_index-1,0)
 
     def send_equation(self,equation):
-        if self.root.ids.textarea.ids.text_field.text != "":
-            self.root.ids.textarea.ids.text_field.focus = False
-            loading_popup = MDDialog(title='Загружаем данные',text='Загрузка...')
-            loading_popup.open()
+        if equation != "":
+            try:
+                #Преобразуем уравнение
+                res = eval(str(equation).replace("√","sqrt").replace("π","math.pi").replace("sin","math.sin").replace("cos","math.cos").replace("tan","math.tan"))
+                self.root.ids["equation_label"].text = str(equation)
+                self.root.ids["equ_type_label"].text = "Решить пример:"
+                self.root.ids["solution_label"].text = str(res)
+                self.set_screen("sc_solve","Решение")
 
-            params = urllib.parse.urlencode({'src':equation})
-            headers = {'Content-type': 'application/x-www-form-urlencoded','Accept': 'text/plain'}
+            except:
+                self.root.ids.textarea.ids.text_field.focus = False
+                loading_popup = MDDialog(title='Загружаем данные',text='Загрузка...')
+                loading_popup.open()
 
-            def success(req, result):
-                loading_popup.dismiss()
-                status_code = result['status_code']
-                codes_list = {"equation":"Решить уравнение","digital":"Решить пример"}
-                if status_code == 0:
-                    res = result["message"]
-                    equ_type = result['type']
-                    self.set_screen("sc_solve","Решение")
-                    
-                    if "plot" in result.keys():
-                        plot_filename = render_plot(result['plot'])
-                        if plot_filename != None:
-                            self.root.ids["plot_img"].source = plot_filename
-                            self.root.ids['plot_card'].visible = True
+                params = urllib.parse.urlencode({'src':equation})
+                headers = {'Content-type': 'application/x-www-form-urlencoded','Accept': 'text/plain'}
+
+                def success(req, result):
+                    loading_popup.dismiss()
+                    status_code = result['status_code']
+                    codes_list = {"equation":"Решить уравнение","digital":"Решить пример"}
+                    if status_code == 0:
+                        res = result["message"].replace("sqrt","√").replace("pi","π")
+                        equ_type = result['type']
                         
-                    else:
-                        self.root.ids['plot_card'].visible = False
+                        if "plot" in result.keys():
+                            plot_filename = render_plot(result['plot'])
+                            if plot_filename != None:
+                                self.root.ids["plot_img"].source = plot_filename
+                                self.root.ids['plot_card'].visible = True
 
-                    self.root.ids["equation_label"].text = equation
-                    self.root.ids["equ_type_label"].text = codes_list[equ_type]+":"
-                    self.root.ids["solution_label"].text = res
-    
-                else:
-                    err = f"\n\n{result}" if self.settings["debug_mode"] == True else ""
-                    popup = MDDialog(title='Ошибка',text=f'Не удалось решить задачу, проверьте правильность введённых данных{err}',buttons=[MDFlatButton(text="Закрыть",theme_text_color="Custom",text_color=self.main_col,on_release=lambda *args:popup.dismiss())])
-                    popup.open()     
+                            else:self.root.ids['plot_card'].visible = False
+                        else:self.root.ids['plot_card'].visible = False
 
-            def error(req, result):
-                loading_popup.dismiss()
-                result = str(result).replace("\n","")
-                err = f"\n\n{result}" if self.settings["debug_mode"] == True else ""
-                popup = MDDialog(title='Ошибка',text=f'Не удаётся получить ответ от сервера,\nпроверьте подключение к интернету{err}',buttons=[MDFlatButton(text="Закрыть",theme_text_color="Custom",text_color=self.main_col,on_release=lambda *args:popup.dismiss())])
-                popup.open()
-            
-            req = UrlRequest(MATH_API_URL,on_success=success,on_failure=error,on_error=error,req_body=params,req_headers=headers,ca_file=certifi.where(),verify=True,method='POST')
+                        self.root.ids["equation_label"].text = equation
+                        self.root.ids["equ_type_label"].text = codes_list[equ_type]+":"
+                        self.root.ids["solution_label"].text = res
+
+                        self.set_screen("sc_solve","Решение")
         
+                    else:
+                        err = f"\n\n{result}" if self.settings["debug_mode"] == True else ""
+                        popup = MDDialog(title='Ошибка',text=f'Не удалось решить задачу, проверьте правильность введённых данных{err}',buttons=[MDFlatButton(text="Закрыть",theme_text_color="Custom",text_color=self.main_col,on_release=lambda *args:popup.dismiss())])
+                        popup.open()     
+
+                def error(req, result):
+                    loading_popup.dismiss()
+                    result = str(result).replace("\n","")
+                    err = f"\n\n{result}" if self.settings["debug_mode"] == True else ""
+                    popup = MDDialog(title='Ошибка',text=f'Не удаётся получить ответ от сервера,\nпроверьте подключение к интернету{err}',buttons=[MDFlatButton(text="Закрыть",theme_text_color="Custom",text_color=self.main_col,on_release=lambda *args:popup.dismiss())])
+                    popup.open()
+                
+                req = UrlRequest(MATH_API_URL,on_success=success,on_failure=error,on_error=error,req_body=params,req_headers=headers,ca_file=certifi.where(),verify=True,method='POST')
+            
         else:
             self.root.ids.textarea.ids.text_field.error = True
 
@@ -262,8 +263,8 @@ class MathCamera(MDApp):
                 else:
                     self.set_screen("sc_text","Ввести уравнение")
 
-                    #Обрезаем текст, если его длина больше 34 символов
-                    if len(result) > 34: result = result[-34:]
+                    #Обрезаем текст, если его длина больше 50 символов
+                    if len(result) > 50: result = result[-50:]
 
                     self.root.ids['textarea'].text = result
             
