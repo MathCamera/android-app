f038d44a58f82ec1bec0873e183b60755751be7d
Dmitry Karpenko <dkarp3ncko@yandex.ru>
2023-08-10 09:56:14 +0500

    pre-release v0.9.0

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-__version__ = "0.7.8"
+__version__ = "0.9.0"
 
 from kivy.lang import Builder
 from kivy.clock import Clock,mainthread
@@ -6,17 +6,20 @@ from kivy.utils import platform
 from kivy.metrics import dp
 from kivy.network.urlrequest import UrlRequest
 from kivy.core.window import Window
+from kivy.core.clipboard import Clipboard
 from kivy.storage.jsonstore import JsonStore
+from kivy.loader import Loader
+Loader.loading_image = "media/loader.png"
 
 from kivymd.app import MDApp
 from kivymd.uix.dialog import MDDialog
-from kivymd.uix.button import MDFlatButton
-from kivymd.uix.menu import MDDropdownMenu
+from kivymd.uix.button import MDFlatButton,MDRectangleFlatIconButton
 from kivymd.uix.list import TwoLineListItem,MDList
 from kivymd.uix.label import MDLabel
+from kivymd.uix.card import MDCard
+from kivymd.uix.fitimage import FitImage
 
 from modules.android_api import request_camera_permission,set_orientation
-from modules.plotting import render_plot
 from modules.xcamera import *
 
 import base64,os,certifi,urllib.parse,json,webbrowser,shutil
@@ -25,7 +28,7 @@ from io import BytesIO
 
 if platform == "android":
     from androidstorage4kivy import Chooser,SharedStorage
-    from kvdroid.tools import change_statusbar_color,navbar_color,toast
+    from kvdroid.tools import change_statusbar_color,toast
     from kvdroid.tools.darkmode import dark_mode
     change_statusbar_color("#02714C", "white")
 else:
@@ -47,21 +50,15 @@ class MathCamera(MDApp):
             self.set_settings(reset=True)
 
         self.history = JsonStore(os.path.join(self.data_dir,'history.json'))
-        self.config_ = json.load(open('data/config.json'))
+        #self.config_ = json.load(open('data/config.json'))
         self.theme_cls.colors = json.load(open('data/themes.json'))
 
         self.theme_cls.material_style = "M2"
         self.main_colors = ["#02714C","#039866"]
 
-        self.last_screen,self.solver_type,self.flashlight_mode = None,None,"off"
-        self.flashlight_modes = {"on":"flash","off":"flash-off"}
-        self.menu_items = {"digital":"Решить пример","equation":"Решить уравнение","system":"Решить систему уравнений","simplify":"Упростить выражение","inequality":"Решить неравенство"}
+        self.last_screen,self.flashlight_mode,self.config_ = None,"off",{'config_url':"https://mathcamera-api.vercel.app/config"}
 
-    def build(self):
-        if platform == "android":
-            self.theme_cls.theme_style = "Dark" if dark_mode() == True else "Light"
-            self.chooser = Chooser(self.chooser_callback)
-        
+    def build(self):        
         return Builder.load_file('data/md.kv')
     
     def set_settings(self,reset=False):
@@ -70,38 +67,47 @@ class MathCamera(MDApp):
 
         with open(os.path.join(self.data_dir,'settings.json'),"w") as file:
             file.write(json.dumps(self.settings))
+
+    def handle_switch(self,type,state):
+        self.settings[type] = state
+        self.set_settings()
     
     def update_config(self):
         def success(req,result):
-            result = json.loads(result)
-            self.config_["math_solve_url"] = result["math_solve_url"]
-            self.config_["ocr_url"] = result["ocr_url"]
+            for elem in result.keys():
+                self.config_[elem] = result[elem]
         
             if __version__ != result["latest_version"]:
-                download_url = result["download_url"]
                 popup = MDDialog(title='Доступно новое обновление',text=f'Вы хотите обновить приложение?',buttons=[MDFlatButton(text="Обновить",theme_text_color="Custom",text_color=self.main_colors[0],on_release=lambda *args:launch_update()),MDFlatButton(text="Закрыть",theme_text_color="Custom",text_color=self.main_colors[0],on_release=lambda *args:popup.dismiss())])
                 popup.open()  
 
                 def launch_update():
-                    webbrowser.open(download_url)
-            
-            with open("data/config.json","w") as file:
-                file.write(json.dumps(self.config_))           
+                    webbrowser.open(result["download_url"])  
 
-        req = UrlRequest(self.config_['config_url'],on_success=success,req_headers={'Content-type': 'application/x-www-form-urlencoded','Accept': 'text/plain'},ca_file=certifi.where(),verify=True,method='GET')
+        def error(req, result):
+            result = str(result).replace("\n","")
+            err = f"\n\n{result}" if self.settings["debug_mode"] == True else ""
+            popup = MDDialog(title='Ошибка',text=f'Не удаётся получить ответ от сервера,\nпроверьте подключение к интернету и повторите попытку{err}',buttons=[MDFlatButton(text="Закрыть",theme_text_color="Custom",text_color=self.main_colors[0],on_release=lambda *args:popup.dismiss()),MDFlatButton(text="Повторить",theme_text_color="Custom",text_color=self.main_colors[0],on_release=lambda *args:retry())])
+            popup.open()  
+            
+            def retry():
+                popup.dismiss()
+                self.update_config()
+
+        req = UrlRequest(self.config_['config_url'],on_success=success,on_failure=error,on_error=error,req_body=urllib.parse.urlencode({'version':self.version_}),req_headers={'Content-type': 'application/x-www-form-urlencoded','Accept': 'text/plain'},ca_file=certifi.where(),verify=True,method='POST')
 
     def on_start(self):
         request_camera_permission()
         set_orientation()
-        Window.bind(on_keyboard=self.key_handler)
         self.update_config()
-        Clock.schedule_once(self.connect_camera)
+        Window.bind(on_keyboard=self.key_handler)
+        
+        if platform == "android":
+            self.theme_cls.theme_style = "Dark" if dark_mode() == True else "Light"
+            self.chooser = Chooser(self.chooser_callback)
 
     def on_stop(self):
         self.root.ids.preview.disconnect_camera()
-
-    def connect_camera(self,dt):
-        self.root.ids.preview.connect_camera(enable_video = False,filepath_callback=self.handle_image)
 
     def chooser_callback(self, shared_file_list):
         ss = SharedStorage()
@@ -131,7 +137,7 @@ class MathCamera(MDApp):
 
     def set_kb(self,kb_name):
         kb_manager = self.root.ids.kb_manager
-        screens_list = ["main_kb","letters_kb","functions_kb"]
+        screens_list = ["main_kb","trigonometry_kb","letters_kb","functions_kb"]
         if kb_name == "next":
             if screens_list.index(kb_manager.current)+1 < len(screens_list):
                 next_screen = screens_list[screens_list.index(kb_manager.current)+1]
@@ -146,19 +152,23 @@ class MathCamera(MDApp):
     def load_history(self):
         history_layout = self.root.ids.history_layout
         history_layout.clear_widgets()
+        try:
+            if len(self.history) != 0:
+                self.root.ids.history_clear_btn.disabled = False
+                history_sw = MDList()
+                self.root.ids.history_layout.add_widget(history_sw)
 
-        if len(self.history) != 0:
-            self.root.ids.history_clear_btn.disabled = False
-            history_sw = MDList()
-            self.root.ids.history_layout.add_widget(history_sw)
-            for elem in self.history:
-                equ_type = self.history[elem]['equ_type']
-                equ_text = elem
-                f = lambda s:self.send_equation(s.secondary_text,list(self.menu_items.keys())[list(self.menu_items.values()).index(s.text)],from_history=True)
-                history_sw.add_widget(TwoLineListItem(text=self.menu_items[equ_type],secondary_text=equ_text,on_release=f))
-        else:
-            self.root.ids.history_clear_btn.disabled = True
-            history_layout.add_widget(MDLabel(text="Пусто",halign="center",font_style="H5"))
+                for elem in self.history:
+                    equ_type = self.history[elem]['equ_type']
+                    equ_text = elem
+                    f = lambda s:self.send_equation(s.secondary_text,from_history=True)
+                    history_sw.add_widget(TwoLineListItem(text=equ_type,secondary_text=equ_text,on_release=f))
+            else:
+                self.root.ids.history_clear_btn.disabled = True
+                history_layout.add_widget(MDLabel(text="Пусто",halign="center",font_style="H5"))
+        except:
+            self.clear_history()
+            self.set_screen("sc_history")
 
     def clear_history(self):
         for elem in self.history:
@@ -191,16 +201,22 @@ class MathCamera(MDApp):
         self.root.ids.preview.capture_photo(location="private",subdir="camera_tmp")
 
     def handle_camera(self):
+        flashlight_modes = {"on":"flash","off":"flash-off"}
+        self.root.ids.preview.connect_camera(enable_video = False,filepath_callback=self.handle_image)
         state = self.root.ids.preview.flash(self.flashlight_mode)
-        self.root.ids.flashlight_btn.icon = self.flashlight_modes[state]
+        self.root.ids.flashlight_btn.icon = flashlight_modes[state]
 
     def switch_flashlight_mode(self):
+        flashlight_modes = {"on":"flash","off":"flash-off"}
         self.flashlight_mode = "on" if self.flashlight_mode == "off" else "off"
         icon = self.root.ids.preview.flash(self.flashlight_mode)
-        self.root.ids.flashlight_btn.icon = self.flashlight_modes[icon]
+        self.root.ids.flashlight_btn.icon = flashlight_modes[icon]
 
     def open_browser(self,url):
         webbrowser.open(url)
+
+    def copy_content(self,text):
+        Clipboard.copy(text)
 
     def set_screen(self,screen_name,*screen_title):
         if self.root.current == "main_sc":
@@ -235,76 +251,62 @@ class MathCamera(MDApp):
 
         self.root.ids.textarea.ids.text_field.cursor = (cursor_pos,0)
 
-    def on_text_screen(self):
-        self.root.ids.textarea.ids.text_field._hide_cut_copy_paste()
-        menu_items = [
-            {
-                "text": i,
-                "on_release": lambda x=i: set_item(x),
-            } for i in self.menu_items.values()]
-
-        self.menu = MDDropdownMenu(
-            caller=self.root.ids.drop_item,
-            items=menu_items,
-            position="bottom",
-            border_margin=24,
-        )
-        self.menu.bind()
-
-        def set_item(text_item):
-            self.root.ids.drop_item.set_item(text_item)
-            self.menu.dismiss()
-            self.solver_type = list(self.menu_items.keys())[list(self.menu_items.values()).index(text_item)]
-
     def send_equation(self,*args,from_history=False):
-        if args:
-            equation = args[0]
-            solver_type = args[1]
-        else:
-            equation = self.root.ids.textarea.ids.text_field.text
-            solver_type = self.solver_type
+        equation = args[0] if args else self.root.ids.textarea.ids.text_field.text
 
-        if equation != "" and solver_type!= None:
+        if equation != "":
             self.root.ids.textarea.ids.text_field.focus = False
 
-            loading_popup = MDDialog(title='Загружаем данные',text='Загрузка...',auto_dismiss=False)
+            loading_popup = MDDialog(title='Загрузка',text='Ожидаем ответ от сервера...',auto_dismiss=False)
             loading_popup.open()
 
-            params = urllib.parse.urlencode({'src':equation,"type":solver_type})
+            params = urllib.parse.urlencode({'src':equation})
 
             def success(req, result):
                 loading_popup.dismiss()
                 status_code = result['status_code']
+
                 if status_code == 0:
-                    
-                    if "plot" in result.keys():
-                        plot_filename = render_plot(result['plot'])
-                        if plot_filename != None:
-                            self.root.ids["plot_img"].source = plot_filename
-                            self.root.ids['plot_card'].visible = True
-                            self.root.ids.gl_sw.do_scroll_y = True
+
+                    gamma_result = result['message']
+                    self.root.ids.gl.clear_widgets()
+
+                    for card in gamma_result:
+                        contains_plot = 'card' in list(card.keys()) and card['card'] == "plot"
+                        contains_latex = 'pre_output' in list(card.keys()) and card['pre_output'] != card['var'] and card['pre_output'] != "\\mathtt{\\text{}}"
+                        
+                        kv_card = MDCard(orientation="vertical",pos_hint={"top":1},md_bg_color=self.main_colors[1],padding=[30,15,30,15],size_hint_y=None,spacing=10)
+                        title_label = MDLabel(text=f"{card['title']}:",adaptive_height=True,theme_text_color="Custom",text_color="white",font_style="H6")
+                        kv_card.add_widget(title_label)
+
+                        if contains_plot == True:
+                            plot_url = self.config_["plotting_url"].format(str(card['input']).replace(" ",""))
+                            image_widget = FitImage(source=plot_url,pos_hint={"center_x":.5},size_hint=(1.3,3.5))
+                            kv_card.height = dp(kv_card.height*1.4)+dp(image_widget.height*1.4)
+                            kv_card.add_widget(image_widget)
+
+                        elif contains_latex == True:
+                            plot_url = self.config_["latex_url"].format(str(card['pre_output']).replace(" ",""))
+                            image_widget = FitImage(source=plot_url,pos_hint={"center_x":.5})
+                            kv_card.height = dp(image_widget.height*1.2)
+                            kv_card.add_widget(image_widget)
 
                         else:
-                            self.root.ids['plot_card'].visible = False
-                            self.root.ids.gl_sw.do_scroll_y = False
-                    else:
-                        self.root.ids['plot_card'].visible = False
-                        self.root.ids.gl_sw.do_scroll_y = False
+                            kv_card.adaptive_height = True
+                            result_text = card['output'] if 'var' not in list(card.keys()) or card['var']=='None' else f"{card['var']} = {card['output']}"
+                            output_label = MDLabel(text=result_text,adaptive_height=True,theme_text_color="Custom",text_color="white")
+                            kv_card.add_widget(output_label)
 
-                    self.root.ids["equation_label"].text = equation
-                    self.root.ids["solution_label"].text = result["message"]
+                        self.root.ids.gl.add_widget(kv_card)
 
                     self.set_screen("sc_solve")
-                    self.root.ids.gl_sw.scroll_y=1
                     
-                    self.root.ids["equ_type_label"].text = self.menu_items[solver_type]
-
                     if from_history == False:
-                        self.history[equation] = {"equ_type":solver_type}
-    
+                        self.history[equation] = {"equ_type":gamma_result[0]['title']}
+
                 else:
                     err = f"\n\n{result}" if self.settings["debug_mode"] == True else ""
-                    popup = MDDialog(title='Ошибка',text=f'Не удалось решить задачу, проверьте правильность введённых данных{err}',buttons=[MDFlatButton(text="Помощь",theme_text_color="Custom",text_color=self.main_colors[0],on_release=lambda *args:webbrowser.open(self.config_["help_url"]+solver_type)),MDFlatButton(text="Закрыть",theme_text_color="Custom",text_color=self.main_colors[0],on_release=lambda *args:popup.dismiss())])
+                    popup = MDDialog(title='Ошибка',text=f'Не удалось решить задачу, проверьте правильность введённых данных{err}',buttons=[MDFlatButton(text="Закрыть",theme_text_color="Custom",text_color=self.main_colors[0],on_release=lambda *args:popup.dismiss())])
                     popup.open()   
 
             def error(req, result):
@@ -330,7 +332,7 @@ class MathCamera(MDApp):
         return file_contents
 
     def send_b64(self,b64):
-        loading_popup = MDDialog(title='Загружаем данные',text='Загрузка...',auto_dismiss=False)
+        loading_popup = MDDialog(title='Загрузка',text='Ожидаем ответ от сервера...',auto_dismiss=False)
         loading_popup.open()
 
         params = urllib.parse.urlencode({'src':b64})
